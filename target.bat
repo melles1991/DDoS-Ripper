3068741b31dce3ac7e4fe5731fc8be87568d5c23
AlexNDRmac <alexndr.mac@gmail.com>
2022-03-08 12:57:45 +0200

    Show current country in stats

--- target.bat
+++ target.bat
@@ -1,7 +1,7 @@
 FOR %%i IN (%IP%) DO (
 FOR %%p IN (%PORT%) DO (
 FOR %%m IN (%METHOD%) DO (
-start "%%i | %%p" python DRipper.py -s %%i -p %%p -t 100 -m %%m -l 2048
+start "%%i | %%p" python -u DRipper.py -s %%i -p %%p -t %THEAD% -m %%m -l 2048
 )
 )
 )
