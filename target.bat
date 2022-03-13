af598a5e240cbcbb01dcb1082c222ca13e7f65d4
Galitsky Dmytro <melles.android@gmail.com>
2022-03-13 19:07:32 +0200

    Fix start http method

--- target.bat
+++ target.bat
@@ -1,6 +1,6 @@
 for %%i in (%IP%) DO (
 for %%p in (%PORT%) DO (
-if %METHOD% == "http" (
+if "%METHOD%" == "http" (
 start "%%i | %%p" python -u DRipper.py -s %%i -p %%p -r 1 -t %THEAD% -m %METHOD%
 ) else (
 start "%%i | %%p" python -u DRipper.py -s %%i -p %%p -r 1 -t %THEAD% -m %METHOD% -l 2048
